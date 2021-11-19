6399053687f348bf69cadbdbe7e85085746898d0
asaravanan42 <saravanan.a@madstreetden.com>
2021-11-19 10:24:32 +0530

    ...

--- embed_code.js
+++ embed_code.js
@@ -1,7 +1,5 @@
-import "./styles.css";
 
 alert();
-
 var my_awesome_script = document.createElement('script');
 my_awesome_script.setAttribute('src','./ui.js');
 document.body.appendChild(my_awesome_script);
